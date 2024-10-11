1f01cefbdbf2a1177f6fc469ccc0553183887b63
luanafelipe <147085629+luanafelipe@users.noreply.github.com>
2024-10-11 09:30:08 -0300

    .

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -5,7 +5,7 @@ async function vizualizarinformacoesGlobais() {
     const dados = await res.json()
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container_texto')
-    paragrafo.innerHTML= voce sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas
+    paragrafo.innerHTML=` voce sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estao conectadas em alguma rede social e passam em média ${dados.tempo_médio} horas conectadas.``
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
